bb6d3d96c576d257ddf6eb0938c13a86c1d67251
ishikaagrawal2903 <ishikaagrawal2903@gmail.com>
2021-09-02 21:18:40 +0530

    add getDate module

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const date = require(__dirname + "/date.js");
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-var workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -11,21 +12,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-    
-    var options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
-    
-    var day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render("list.ejs", {listTitle : day, newListItems : items});
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    const item = req.body.newItem;
 
     if(req.body.list==="Work"){
         workItems.push(item);
